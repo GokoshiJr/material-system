def2297e80145930456a85ec0b69407845369950
GokoshiJr <julioagonzalez18@gmail.com>
2022-06-01 17:49:18 -0400

    feat: Update login logic and show alerts

--- src/sections/auth/login/LoginForm.js
+++ src/sections/auth/login/LoginForm.js
@@ -1,23 +1,32 @@
 import * as Yup from 'yup';
-import { useState } from 'react';
-import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+import { useState, useContext } from 'react';
+import { useNavigate, Link as RouterLink } from 'react-router-dom';
 import { useFormik, Form, FormikProvider } from 'formik';
 // material
 import { Link, Stack, Checkbox, TextField, IconButton, InputAdornment, FormControlLabel } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
 // component
 import Iconify from '../../../components/Iconify';
-
+// Context
+import { AppContext } from '../../../context/AppContext'
 // ----------------------------------------------------------------------
 
-export default function LoginForm() {
-  const navigate = useNavigate();
+const API_URL = process.env.REACT_APP_API_URL;
 
+export default function LoginForm() {
+  const Auth = useContext(AppContext);
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
 
   const LoginSchema = Yup.object().shape({
-    email: Yup.string().email('Email must be a valid email address').required('Email is required'),
-    password: Yup.string().required('Password is required'),
+    email: Yup.string()
+      .email('Email must be a valid email address')
+      .required('Email is required'),
+    password: Yup.string()
+      .min(8,'Must be almost 8 characters')
+      .required('Password is required'),
   });
 
   const formik = useFormik({
@@ -28,11 +37,46 @@ export default function LoginForm() {
     },
     validationSchema: LoginSchema,
     onSubmit: () => {
-      navigate('/dashboard', { replace: true });
+      login();
     },
   });
-
-  const { errors, touched, values, isSubmitting, handleSubmit, getFieldProps } = formik;
+  
+  const { 
+    setErrors, 
+    errors, 
+    touched, 
+    values, 
+    isSubmitting, 
+    setSubmitting, 
+    handleSubmit, 
+    getFieldProps 
+  } = formik;  
+  
+  const login = () => {
+    // form values
+    const { email, password } = values
+    // api sync
+    axios.post(`${API_URL}/auth/signin`, { email, password })
+    .then(({data}) => {
+      localStorage.setItem("token", data.token);
+      Auth.setToken(data.token)
+      navigate('/dashboard/app', { replace: true });
+    })
+    .catch((err) => {
+      setSubmitting(false);
+      const { status: text, type } = err.response.data;
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text
+      });
+      if (type === 'email') {
+        setErrors({ email: text });
+      } else {
+        setErrors({ password: text });
+      }
+    })
+  }
 
   const handleShowPassword = () => {
     setShowPassword((show) => !show);
