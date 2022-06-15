544ef64000b6e7c28190ce12eff3c094584b0cb1
Julio Gonzalez <julioagonzalez18@gmail.com>
2022-06-15 16:12:22 -0400

    feat: Add updateLoggedUser method in api

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,21 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+async function updateLoggedUser(token, user) {
+  try {
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/user/updateLoggedUser`,
+      method: 'POST',
+      data: user
+    })
+    return res; 
+  } catch (err) {
+    console.log(err)
+    return null;
+  }
+}
+
 async function getMe(token) {
   try {
     const res = await axios({
@@ -20,7 +35,7 @@ async function getUsers(token) {
   try {
     const res = await axios({
       headers: {"x-access-token": token},
-      url: `${API_URL}/products/getMe`,
+      url: `${API_URL}/user `,
       method: 'GET'
     })
     return res;
@@ -33,5 +48,6 @@ async function getUsers(token) {
 
 export {
   getMe,
-  getUsers
+  getUsers,
+  updateLoggedUser
 }
