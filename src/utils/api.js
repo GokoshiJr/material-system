3b0a19a76c658cc0f5675bf1e023ab1f80b8305f
Julio Gonzalez <julioagonzalez18@gmail.com>
2022-08-27 17:25:47 -0400

    fix: Update api methods

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,6 +2,83 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
+/* actualiza un empleado */
+async function updateClient(token, id, client) {
+  try {
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/client/${id}`,
+      method: 'PUT',
+      data: client
+    })
+    return { title: res.data.status, icon: 'success' };
+  } catch (err) {
+    console.log(err)
+    return { title: err.response.statusText, icon: 'error' } ;
+  }
+}
+
+/* muestra un empleado por su id */
+async function showClient(token, id) {
+  try {
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/client/${id}`,
+      method: 'GET'
+    })
+    return res;
+  } catch (err) {
+    console.log(err)
+    return null;
+  }
+}
+
+/* crea un empleado y su usuario */
+async function createClient(token, client) {
+  try {
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/client`,
+      method: 'POST',
+      data: client
+    });
+    return { title: res.data.status, icon: 'success' };
+  } catch (err) {
+    console.log(err)
+    return { title: err.response.statusText, icon: 'error' }
+  }
+}
+
+/* elimina un client por su id */
+async function eliminateClient(token, id) {
+  try {
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/client/${id}`,
+      method: 'DELETE'
+    })
+    return res;
+  } catch (err) {
+    console.log(err)
+    return null;
+  }
+}
+
+/* retorna una lista de todos los clients */
+async function getClients(token) {
+  try {
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/client`,
+      method: 'GET'
+    })
+    return res;
+  } catch (err) {
+    console.log(err)
+    return null;
+  }
+}
+
 /* crea un empleado y su usuario */
 async function createEmployee(token, employee) {
   try {
@@ -124,6 +201,11 @@ async function getUsers(token) {
 
 
 export {
+  updateClient,
+  showClient,
+  createClient,
+  eliminateClient,
+  getClients,
   createEmployee,
   updateEmployee,
   showEmployee,
