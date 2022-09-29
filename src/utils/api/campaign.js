49de90b2b124729d5fdb1e8a400eea4e323e9e08
GokoshiJr <julioagonzalez18@gmail.com>
2022-09-29 14:35:00 -0400

    fix: Add updateBalanceInProjection api req

--- src/utils/api/campaign.js
+++ src/utils/api/campaign.js
@@ -3,6 +3,25 @@ import axios from 'axios';
 const API_URL = process.env.REACT_APP_API_URL;
 
 // return client by campaign id
+async function updateBalanceInProjection(token, projectionId, balances) {
+  try {
+    console.log(balances)
+    const res = await axios({
+      headers: {"x-access-token": token},
+      url: `${API_URL}/projection/${projectionId}`,
+      method: 'PUT',
+      data: {
+        balances
+      }
+    })
+    return res;
+  } catch (err) {
+    console.log(err)
+    return null;
+  }
+}
+
+// return client by campaign id
 async function clientInCampaign(token, campaignId) {
   try {
     const res = await axios({
@@ -48,6 +67,7 @@ async function show(token, id) {
 }
 
 export {
+  updateBalanceInProjection,
   clientInCampaign,
   index,
   show
